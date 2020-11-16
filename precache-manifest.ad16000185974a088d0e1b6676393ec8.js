4dcf398a46810e9017a6c119b96c193eb4dd1590
Derek_DZ <a956997493@gmail.com>
2020-11-16 09:53:52 +0800

    deploy

---
+++ precache-manifest.ad16000185974a088d0e1b6676393ec8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7adf63c18ddef3f6e908",
+    "url": "/account-project-website/css/app.17de527a.css"
+  },
+  {
+    "revision": "376c8148c3855403b37fd9d428e72068",
+    "url": "/account-project-website/images/QRcode.png"
+  },
+  {
+    "revision": "f40aa160855bd24ca78440d9dc4e7a8f",
+    "url": "/account-project-website/index.html"
+  },
+  {
+    "revision": "7adf63c18ddef3f6e908",
+    "url": "/account-project-website/js/app.95859989.js"
+  },
+  {
+    "revision": "7647cea12a98e5207c88",
+    "url": "/account-project-website/js/chunk-vendors.f8409545.js"
+  },
+  {
+    "revision": "f2ef55801f65a0060f1763de4116eca1",
+    "url": "/account-project-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/account-project-website/robots.txt"
+  }
+]);
